f9cd021653fc32bb1beb95bf07cdd18ce1c1adc0
João <joaomanoeldev@gmail.com>
2022-08-08 17:24:04 -0300

    create component header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <Transactions />
 
       <GlobalStyle />
     </ThemeProvider>
