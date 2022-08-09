79dde7969657f7d3f170e011d5eeb0d5bc926d33
João <joaomanoeldev@gmail.com>
2022-08-09 12:30:04 -0300

    creating hook for the summary

--- web/src/components/Summary/index.tsx
+++ web/src/components/Summary/index.tsx
@@ -3,29 +3,11 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { useTheme } from 'styled-components'
 import { useTransactions } from '../../hooks/useTransactions'
 import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
   const { colors } = useTheme()
-  const { transactions } = useTransactions()
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
