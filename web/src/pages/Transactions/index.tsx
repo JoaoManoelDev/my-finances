0328d6bebaca791bcb1b2ad16bf08748798a80d1
João <joaomanoeldev@gmail.com>
2022-08-08 23:32:09 -0300

    create component searchForm

--- web/src/pages/Transactions/index.tsx
+++ web/src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
 import {
   PriceHighLight,
   TransactionsContainer,
@@ -11,8 +12,8 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
-
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
