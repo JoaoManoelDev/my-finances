cfcd5b0cee5fe0dbd584170d50589fcec3120b01
João <joaomanoeldev@gmail.com>
2022-08-08 23:01:14 -0300

    create transaction table

--- web/src/pages/Transactions/index.tsx
+++ web/src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>08/08/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 50,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>08/08/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
